c08a10924078a7570289a080f820f8de1b9c3214
Rowan Krishnan <rkrishnan8594@gmail.com>
2016-02-04 10:52:50 -0500

    Configured postgres

--- IncidentTracker/app/controllers/home_controller.rb
+++ IncidentTracker/app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
-  :require_authentication
 	def index
-    if user_signed_in?
-      @user = current_user
-    end
+    @user = current_user
   end
 end
