e3b4af57336cbd578efce465a63547987e9f0a5d
Rowan Krishnan <rkrishnan8594@gmail.com>
2016-02-15 01:04:19 -0500

    Lots of styling

--- IncidentTracker/db/schema.rb
+++ IncidentTracker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213205554) do
+ActiveRecord::Schema.define(version: 20160213214520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 20160213205554) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name",                           null: false
+    t.string   "role",                                null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
