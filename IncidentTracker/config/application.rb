25b80016cf0fafcc6de0246edf73c02013e08f47
Rowan Krishnan <rkrishnan8594@gmail.com>
2016-03-03 18:02:09 -0500

    Expiration fix

--- IncidentTracker/config/application.rb
+++ IncidentTracker/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module IncidentTracker
   class Application < Rails::Application
+    config.static_cache_control = "public, max-age=31536000"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
