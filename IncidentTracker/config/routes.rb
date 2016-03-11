5971d6082749d221a8940e3de3285236d11ab8d7
Rowan Krishnan <rkrishnan8594@gmail.com>
2016-03-11 00:56:14 -0500

    Its working

--- IncidentTracker/config/routes.rb
+++ IncidentTracker/config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :incidents
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "routes#index"
-  mount API::Base => "/api"
-  mount GrapeSwaggerRails::Engine, at: "/documentation"
+  mount API::Base, at: "/"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
