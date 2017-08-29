1dcb2ed19a07b0d439fa8447030ff1cdd2c75c63
Rowan Krishnan <rkrishnan8594@gmail.com>
2017-08-29 03:12:50 -0400

    More cleanup

--- detect_faces.py
+++
@@ -1,8 +0,0 @@
-import sys
-import cv2
-cap = cv2.VideoCapture(sys.argv[1])
-if( cap.isOpened() ) :
-    ret,img = cap.read()
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-faces = face_cascade.detectMultiScale(img, 1.3, 5, cv2.cv.CV_HAAR_SCALE_IMAGE)
-print len(faces)
